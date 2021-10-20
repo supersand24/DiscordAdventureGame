c04acbde27c4d7b941dd77f753c2b06a2861a398
supersand24 <supersand24@gmail.com>
2021-10-20 14:54:06 -0400

    Update Goblin.java

--- src/main/java/Game/Entities/EnemyTypes/Grunts/Goblin.java
+++ src/main/java/Game/Entities/EnemyTypes/Grunts/Goblin.java
@@ -3,7 +3,7 @@ package Game.Entities.EnemyTypes.Grunts;
 /**
  * enemy goblin class
  * @author Harrison Brown
- * @version 0.1
+ * @version 0.1.1
  */
 public class Goblin extends Grunt {
     /**
@@ -12,6 +12,6 @@ public class Goblin extends Grunt {
      */
     public Goblin() {
         super();
-        this.name = name;
+        this.name = "Goblin";
     }
 }
