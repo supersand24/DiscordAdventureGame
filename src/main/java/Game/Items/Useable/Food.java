b9bace42bc6910300d3c53b6c82051509bd3a027
Harrison Brown <70150559+sqKweglord@users.noreply.github.com>
2021-10-21 01:11:02 -0400

    javadocs for all items

--- src/main/java/Game/Items/Useable/Food.java
+++ src/main/java/Game/Items/Useable/Food.java
@@ -2,16 +2,37 @@ package Game.Items.Useable;
 
 import Game.Entities.Entity;
 
+/**
+ * a basic food class
+ * @author Harrison Brown
+ * @version 0.1
+ *
+ * will probably be changed to abstract as more food is added
+ */
 public class Food extends Usable {
+
+    /**
+     * default constructor for food
+     * @author Harrison Brown
+     */
     public Food() {
         super("Food", 1,1);
     }
 
+    /**
+     * override method use from Usable class
+     * @author Harrison Brown
+     */
     @Override
     public void use() {
 
     }
 
+    /**
+     * override for abstract use with entity parameter
+     * @author Harrison Brown
+     * @param e the entity that used the potion
+     */
     @Override
     public void use(Entity e) {
 
--- src/main/java/Game/Items/Useable/Usable.java
+++ src/main/java/Game/Items/Useable/Usable.java
@@ -3,14 +3,26 @@ package Game.Items.Useable;
 import Game.Entities.Entity;
 import Game.Items.Item;
 
+/**
+ * abstract parent class for all items that the player can use
+ * @author Harrison Brown
+ * @version 0.1
+ */
 public abstract class Usable extends Item {
 
+    /**
+     * the max amount of times that an itm can be used
+     */
     protected int maxUses;
 
+    /**
+     * the amount of times that an item can be used
+     */
     protected int uses;
+
     /**
      * sets the name, cost, and weight of an item
-     *
+     * @author Harrison Brown
      * @param name   item name
      * @param cost   item cost
      * @param weight item weight
@@ -20,14 +32,31 @@ public abstract class Usable extends Item {
         super(name, cost, weight);
     }
 
+    /**
+     * default constructor for Usable
+     */
     protected Usable() {
         super();
     }
 
+    /**
+     * set the abstract method for Usable items
+     * @author Harrison Brown
+     * @param e the entity that used the potion
+     */
     public abstract void use(Entity e);
 
+    /**
+     * a use method without parameters
+     * @author Harrison Brown
+     */
     public abstract void use();
 
+    /**
+     * returns the amount of times that an item has left to be used
+     * @author Harrison Brown
+     * @return int
+     */
     public int checkUses() {
         return uses;
     }
